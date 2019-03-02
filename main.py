e1f11211175db722ca25db4e1e82dd76a12742b6
hsauers5 <hsauers5@gmail.com>
2019-03-02 05:35:32 +0000

    Addd yelp api

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Use Flask / etc to design a REST API for WhatsNext back-end.
+# get creds
 GOOGLE_API = ""
 with open("googleapi.txt") as creds:
     GOOGLE_API = creds.readlines()[0]
@@ -6,3 +7,9 @@ with open("googleapi.txt") as creds:
         GOOGLE_API.replace('\n', '')
 print(GOOGLE_API)
 
+YELP_API = ""
+with open("yelpapi.txt") as creds:
+    YELP_API = creds.readlines()[0]
+    if '\n' in YELP_API:
+        YELP_API.replace('\n', '')
+print(YELP_API)
