e2bed9a8b0d2931ea3b4b8d365925fe638ad7d5c
hsauers5 <hsauers5@gmail.com>
2019-03-02 05:33:53 +0000

    Read google api from file.

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 # Use Flask / etc to design a REST API for WhatsNext back-end.
-print("Hello World")
+GOOGLE_API = ""
+with open("googleapi.txt") as creds:
+    GOOGLE_API = creds.readlines()[0]
+    if '\n' in GOOGLE_API:
+        GOOGLE_API.replace('\n', '')
+print(GOOGLE_API)
+
