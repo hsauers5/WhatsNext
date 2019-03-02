70b87bcaed9882d5326a787f773847ddebcfa892
hsauers5 <hsauers5@gmail.com>
2019-03-02 06:05:04 +0000

    Convert miles to meters.

--- yelp.py
+++ yelp.py
@@ -5,7 +5,17 @@ YELP_API = creds.get_yelp_creds()
 
 # finds suggestions via yelp api
 def find_suggestions(location, category, radius, money):
+    # convert into meters for yelp api
+    radius = convert_to_meters(radius)
+
     # dummy suggestions list
     suggestions = {'id':{'name':'thisname'}, 'newid':{'name':'newname'}}
+
     # query yelp api for suggestions
+
     return suggestions
+
+
+# convert to meters for yelp api
+def convert_to_meters(radius):
+    return radius * 1609
