2d80e4562ee63fada8615ad3d25754e9df5571ff
hsauers5 <hsauers5@gmail.com>
2019-03-02 05:38:26 +0000

    Manage credentials as needed.

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
+import creds
+
 # Use Flask / etc to design a REST API for WhatsNext back-end.
 # get creds
-GOOGLE_API = ""
-with open("googleapi.txt") as creds:
-    GOOGLE_API = creds.readlines()[0]
-    if '\n' in GOOGLE_API:
-        GOOGLE_API.replace('\n', '')
-print(GOOGLE_API)
+GOOGLE_API = creds.get_google_creds()
+YELP_API = creds.get_yelp_creds()
 
-YELP_API = ""
-with open("yelpapi.txt") as creds:
-    YELP_API = creds.readlines()[0]
-    if '\n' in YELP_API:
-        YELP_API.replace('\n', '')
-print(YELP_API)
+
